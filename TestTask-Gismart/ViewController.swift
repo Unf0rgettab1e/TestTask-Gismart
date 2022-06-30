00d0fe941f68da28f05723472dc1062e69fe20b5
AnthonyDavydchyk <62406462+AnthonyDavydchyk@users.noreply.github.com>
2022-06-30 20:35:51 +0300

    Initial Commit

---
+++ TestTask-Gismart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask-Gismart
+//
+//  Created by Tony on 30.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
